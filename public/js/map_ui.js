8b72969cb99d27f66d7c2b0ad3c388772c32ccc5
albert deng <albertdeng@kkbox.com>
2019-11-25 18:04:04 +0800

    add map page

---
+++ public/js/map_ui.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+
+	 $(".location_position").find('.location').click(function(){
+
+
+	 	// $('.location_detail').removeClass('show')
+
+	 	$(this).parent().find('.location_detail').toggleClass('show')
+
+	  });
+
+
+
+});
+
+
+
+
+
+
