058f4304e6f389edcf14231bdec83bbab61c4b8c
albertdeng <albertdeng@kkbox.com>
2019-12-02 01:27:21 +0800

    1202

--- public/js/map_ui.js
+++ public/js/map_ui.js
@@ -4,9 +4,12 @@ $(document).ready(function(){
 	 $(".location_position").find('.location').click(function(){
 
 
-	 	// $('.location_detail').removeClass('show')
+	 	// $('.location_detail').removeClass('show');
+	 	$(this).parent().siblings().find('.location_detail').removeClass('show')
 	 	$(this).parent().find('.location_detail').toggleClass('show')
 
+	 	$(this).parent().siblings().css('z-index', 100)
+	 	$(this).parent().css('z-index', 5000)
 	  });
 
 
